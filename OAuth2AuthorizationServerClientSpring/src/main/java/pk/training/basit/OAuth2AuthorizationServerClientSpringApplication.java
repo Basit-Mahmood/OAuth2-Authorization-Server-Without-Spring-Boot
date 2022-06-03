a68f3cad8b820088c644c4cd96eb0969acc1307f
Basit Mahmood Ahmed <basit_mahmood@live.com>
2022-06-03 23:02:14 +0500

    Spring Authorization Server Client

---
+++ OAuth2AuthorizationServerClientSpring/src/main/java/pk/training/basit/OAuth2AuthorizationServerClientSpringApplication.java
@@ -0,0 +1,13 @@
+package pk.training.basit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2AuthorizationServerClientSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2AuthorizationServerClientSpringApplication.class, args);
+	}
+
+}
---
+++ OAuth2AuthorizationServerClientSpring/src/test/java/pk/raining/basit/OAuth2AuthorizationServerClientSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pk.raining.basit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2AuthorizationServerClientSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
